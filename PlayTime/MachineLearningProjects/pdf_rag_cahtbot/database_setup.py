e7e2e37ef162b881b53f900ba5efe31c3b37492d
MIHIRZZ <mihirpatel23793@gmail.com>
2025-07-07 21:29:42 -0400

    added pdf rag chatbot

---
+++ PlayTime/MachineLearningProjects/pdf_rag_cahtbot/database_setup.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv("PG_VECTOR_HOST")
+user = os.getenv("PG_VECTOR_USER")
+password = os.getenv("PG_VECTOR_PASSWORD")
+COLLECTION_NAME = os.getenv("PGDATABASE")
+CONNECTION_STRING = "postgresql+psycopg://langchain:langchain@localhost:6024/langchain"
+#CONNECTION_STRING = f"postgresql+psycopg://{user}:{password}@{host}:6042/{COLLECTION_NAME}"
